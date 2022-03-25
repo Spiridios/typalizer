e919a8cdaf94d64e79a6e0a088dbc1928929732a
Micah <haxmjl@debian-server.legolan.spiridios.com>
2022-03-25 14:43:33 -0700

    Move attack strengths to own column in main table

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var pokeData = new PokeData();
+let valueSymbols = {0.25: "¼", 0.5: "½", 0: "0", 1: "", 2: "2", 4: "4"};
 
 function toggleHighlightClass(classToFind, classToToggle) {
     let tds = document.getElementsByClassName(classToFind);
@@ -18,7 +19,6 @@ function toggleHighlightClass(classToFind, classToToggle) {
 }
 
 function updateAttacks(selectedDefense) {
-    let strongAttacksP = document.getElementById("strongAttacks");
     let attackMap={};
     selectedDefense.forEach(type => {
         let d = pokeData.typeIndices[type];
@@ -31,9 +31,15 @@ function updateAttacks(selectedDefense) {
         }
     });
 
-    strongAttacksP.innerHTML = "";
-    for(const typeIdx in attackMap) {
-        strongAttacksP.innerHTML += pokeData.typeNames[typeIdx] + ": " + attackMap[typeIdx] + "<br/>"
+    for (let typeIdx = 0; typeIdx < pokeData.numTypes; typeIdx ++) {
+        let tds = document.getElementsByClassName(pokeData.typeNames[typeIdx] + "-Combined-Attack");
+        let td = tds[0];
+
+        if (attackMap[typeIdx] != undefined) {
+            td.innerText = valueSymbols[attackMap[typeIdx]];
+        } else {
+            td.innerText = "";
+        }
     }
 }
 
@@ -56,25 +62,24 @@ function attackClick(type) {
 
 
 function createTypesTable() {
-    let valueSymbol = {0.5: "½", 0: "0", 1: "", 2: "2"};
     let attackClass = {0.5: "weak", 0: "immune", 1: "normal", 2: "strong"};
 
     let typesTable = document.getElementById("typesTable");
     let tr = document.createElement("tr");
     let lth = document.createElement("td");
     lth.innerHTML = "Defense →<br/><br/>Attack ⤵";
-    lth.className = "split";
+    lth.className = "Split";
     tr.appendChild(lth);
     lth = document.createElement("td");
     lth.innerHTML = "Combined";
-    lth.className = "defense";
+    lth.className = "Defense";
     tr.appendChild(lth);
     // Defense headers
     for (let i = 0; i < pokeData.numTypes;i++) {
         let defenseTypeName=pokeData.githubData[i].name;
         let th = document.createElement("td");
         th.innerText = defenseTypeName;
-        th.classList.add('defense', defenseTypeName + "-Defense");
+        th.classList.add('Defense', defenseTypeName + "-Defense");
 
         th.onclick = function() {defenseClick(defenseTypeName);};
         tr.appendChild(th);
@@ -87,18 +92,18 @@ function createTypesTable() {
         let attackTypeName = pokeData.typeNames[a];
         let th = document.createElement("td");
         th.innerText = attackTypeName;
-        th.classList.add('attack', attackTypeName + "-Attack");
+        th.classList.add('Attack', attackTypeName + "-Attack");
         th.onclick = function() {attackClick(attackTypeName);};
         tr.appendChild(th);
         th = document.createElement("td");
-        th.classList.add('combined-attack', attackTypeName + "-Attack");
+        th.classList.add('Combined-Attack', attackTypeName + "-Combined-Attack");
         tr.appendChild(th);
 
         for (let d = 0; d < pokeData.numTypes; d++) {
             let defenseTypeName = pokeData.githubData[d].name;
             let td = document.createElement("td");
             let attackValue = pokeData.attackDefenseLookup[a][d];
-            td.innerText = valueSymbol[attackValue];
+            td.innerText = valueSymbols[attackValue];
             td.classList.add(attackClass[attackValue],  attackTypeName + "-" + defenseTypeName, attackTypeName + "-Attack", defenseTypeName + "-Defense");
 
             tr.appendChild(td);
