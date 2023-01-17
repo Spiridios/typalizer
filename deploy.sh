7044c86e7cff10a6c33c2c2638dd1131821f2889
Micah <haxmjl@debian-server.legolan.spiridios.com>
2023-01-17 10:33:25 -0800

    Move code into src folder

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 destination="/home/docker-op/volumes/lanwww/www/typalizer/"
 
-cp *.html "${destination}"
-cp *.js "${destination}"
-cp *.css "${destination}"
+cp src/* "${destination}"
+# cp *.html "${destination}"
+# cp *.js "${destination}"
+# cp *.css "${destination}"
